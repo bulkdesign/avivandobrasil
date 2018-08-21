0ef473084193acbbfe79cf37e304157c472905b2
William Kunitake <wykunitake@Williams-MacBook-Pro.local>
2018-08-21 02:30:11 -0300

    Criação das sections da página inicial. Toda a estrutura definida

--- wp-content/themes/avivando/footer.php
+++ wp-content/themes/avivando/footer.php
@@ -9,16 +9,57 @@
 
 ?>
 
-    </div><!-- .col-full -->
-  </div><!-- #content -->
+  </div><!-- .col-full -->
+</div><!-- #content -->
 
-  <footer class="page-footer">
-      <div class="footer-copyright">
-        <div class="container center">
-          Andando Com Jesus © <?php echo date('Y'); ?> - Todos os direitos reservados | Made by <a class="purple-text text-lighten-3" href="http://www.bulkdesign.com.br" target="blank">Bulk Design</a>
+<div class="almost-black">
+  <div class="container">
+    <div class="row">
+      <div class="col s12">
+        <div class="col s12 l7 margin20 padding40">
+          <p class="white-text bold left-align">SOBRE</p>
+          <p class="white-text left-align">
+            O Avivando Brasil é um portal de informações que deixa você por dentro de tudo que<br>
+          acontece no movimento cristão evangélico do Brasil. Desde informações e localização das<br>
+          principais igrejas até datas de eventos, congressos, treinamentos, bandas e pastores que<br>
+          estarão na sua cidade e nas igrejas.
+          Tudo em um só lugar! Com o Avivando Brasil você fica sempre ligado e não perde nada!
+          Inscreva-se e receba informações semanais! Quem estará tocando e pregando? Onde será?<br>
+          Quais serão os dias e horários? Quanto é a inscrição?<br>
+          Com o Avivando Brasil você por dentro de tudo!
+          </p>
+        </div>
+        <div class="col hide-on-med-and-down l4 right margin20 padding40">
+          <p class="white-text bold right-align">MENU DE NAVEGAÇÃO</p>
+          <ul class="right-align links-rodape">
+            <li><a href="/">HOME</li>
+            <li><a href="/#oquee">IGREJAS</li>
+            <li><a href="/#paraqueserve">CULTOS ONLINE</li>
+            <li><a href="/como-comprar">EVENTOS</li>
+            <li><a href="/medicos">TREINAMENTOS</li>
+            <li><a href="/produtos">SOBRE</li>
+            <li><a href="/contato">CONTATO</li>
+          </ul>
         </div>
       </div>
-  </footer>
+    </div>
+  </div>
+</div>
+<footer class="page-footer">
+  <div class="container">
+    <div class="row">
+          <div class="col s12 padding20">
+            <h3 class="white-text">Avivando Brasil</h3>
+            <a href="/termos-de-uso">Termos de Uso</a> | <a href="/politica-de-privacidade">Política de Privacidade</a> | <a href="/politica-de-devolucao">Política de Devolução</a>
+          </div>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="container">
+      Todos os direitos reservados © 2018
+    </div>
+  </div>
+</footer>
 
 </div><!-- #page -->
 <?php wp_footer(); ?>
@@ -26,43 +67,6 @@
 <script type="text/javascript" src="<?php bloginfo('template_url') ?>/assets/js/filter-control/bootstrap-table-filter-control.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_url');?>/assets/js/funcoes.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_url');?>/assets/js/materialize.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.0/jquery.mask.js"></script>
-<script type="text/javascript">
-
-$(document).ready(function(){
-  $('ul.tabs').tabs('select_tab', 'tab_id');
-  $("input[name=billing_postcode]").blur(function(){
-    var cep = $(this).val();
-    if(cep){
-    var url = 'https://viacep.com.br/ws/' + cep + '/json/';
-      $.ajax({
-        url: url,
-        dataType: 'json',
-        crossDomain: true,
-        contentType: "application/json",
-        success : function(json){
-
-        if(json.logradouro){
-
-          $("input[name=billing_address_1]").val(json.logradouro);
-          $("input[name=billing_neighborhood]").val(json.bairro);
-          $("input[name=billing_city]").val(json.localidade);
-
-          }
-        }
-      });
-    }
-  });
-});
- 
-</script>
-<style type="text/css">
-  
-.storefront-full-width-content .content-area {
-  height: auto !important;
-}
-
-</style>
 
 </body>
 </html>
--- wp-content/themes/avivando/page-home.php
+++ wp-content/themes/avivando/page-home.php
@@ -36,63 +36,135 @@
   </div>
 </div>
 <!-- O QUE É -->
-<div id="oquee" class="parallax-container z-depth-4 hauto">
-  <div class="row">
-    <!-- DESKTOP -->
-    <div class="col hide-on-med-and-down l6 push-l1 left-align">
-      <h1 class="white-text bold">O que é?</h1>
-      <span class="white-text"></span>
+<div id="oquee" class="parallax-container not-so-black z-depth-4 hauto">
+  <div class="container">
+    <div class="row">
+      <!-- DESKTOP -->
+      <div class="col s8 push-s2 margin50 marginb50">
+        <h1 class="white-text bold">Unidade + Jesus = <span class="texto-amarelo">Avivando</span></h1>
+        <h3 class="white-text">Nós cremos que através da unidade da igreja de Cristo nesta terra + um genuíno reconhecimento de nossa identidade e do sacrifício de Jesus na cruz será gerado um avivamento e um despertar de cada um de nós. Avivar é você tornar-se mais vivo, animar-se, avigorar-se, reanimar seu espírito. E para isso é necessário estar ligado à videira verdadeira e produzir frutos. É para isso que o Avivando Brasil foi criado, para te ajudar a manter a chama acesa e contagiar outros.</h3>
+      </div>
     </div>
-    <!-- MED AND DOWN -->
-    <div class="col hide-on-large-only paddings1 s12 m6 push-m1 margin50 marginb50 left-align">
-      <h1 class="white-text bold">O que é?</h1>
-      <span class="white-text"></span>
-    </div>
-  </div>
-  <div class="col hide-on-small-only parallax">
-    <img src="">
-  </div>
-  <div class="col hide-on-med-and-up parallax">
-    <img src="">
   </div>
 </div>
-<!-- PARA QUE SERVE -->
-<div id="paraqueserve" class="parallax-container z-depth-4 hauto">
-  <div class="row">
-    <!-- DESKTOP -->
-    <div class="col hide-on-med-and-down l6 pull-l1 right right-align">
-      <h1 class="white-text bold">Para que serve?</h1>
-      <span class="white-text"></span>
-    </div>
-    <!-- MED AND DOWN -->
-    <div class="col hide-on-large-only paddings1 s12 m6 pull-m1 margin50 marginb50 right right-align">
-      <h1 class="white-text bold">Para que serve?</h1>
-      <span class="white-text"></span>
+<!-- AVIVE VOCÊ TAMBÉM -->
+<div class="hide-on-small-only parallax-container" style="height: 700px">
+  <div class="texto-por-cima-parallax">
+    <div class="row">
+      <div class="col s8 push-s2 margin50 marginb50">
+        <h1 class="texto-amarelo bold">Avive você também!</h1>
+        <h3 class="white-text">
+          O Avivando Brasil é um portal de informações que deixa você por dentro de tudo que<br>
+          acontece no movimento cristão evangélico do Brasil. Desde informações e localização das<br>
+          principais igrejas até datas de eventos, congressos, treinamentos, bandas e pastores que<br>
+          estarão na sua cidade e nas igrejas.
+          <br><br>
+          Tudo em um só lugar! Com o Avivando Brasil você fica sempre ligado e não perde nada!
+          <br><br>
+          Inscreva-se e receba informações semanais! Quem estará tocando e pregando? Onde será?<br>
+          Quais serão os dias e horários? Quanto é a inscrição?<br>
+          Com o Avivando Brasil você por dentro de tudo!
+        </h3>
+        <br>
+        <a href="#" class="btn btn-destaque texto-amarelo">BUSCAR IGREJAS PRÓXIMAS</a>
+      </div>
     </div>
   </div>
-  <div class="col hide-on-small-only parallax">
-    <img src="">
-  </div>
-  <div class="col hide-on-med-and-up parallax">
-    <img src="">
+  <div class="parallax">
+    <img src="<?php bloginfo('template_url'); ?>/images/section3.jpg" />
   </div>
 </div>
-<!-- COMO COMPRAR -->
-<div class="parallax-container">
-  <div class="row">
-    <!-- DESKTOP -->
-    <div class="col hide-on-med-and-down l6 push-l1 margin140 left-align">
-      <h1 class="white-text bold">Como comprar?</h1>
-      <span class="white-text"></span>
-      <a href="/cbdmed/?page_id=28" class="btn green bold">Conheça o processo de compra</a>
+<!-- ENCONTRE AS IGREJAS -->
+<div class="hide-on-small-only parallax-container" style="height: 700px">
+  <div class="container">
+    <div class="row">
+      <div class="col s8 push-s2 margin50 marginb50">
+        <h1 class="padding40 texto-escuro bold">Encontre as Igrejas mais próximas:</h1>
+        <br>
+        <a href="#" class="btn btn-destaque texto-amarelo">Ver todas as Igrejas</a>
+      </div>
     </div>
-    <!-- MED AND DOWN -->
-    <div class="col hide-on-large-only paddings1 s12 m6 push-m1 margin50 marginb50 left-align">
-      <h1 class="white-text bold">Como comprar?</h1>
-      <span class="white-text"></span>
-      <a href="/cbdmed/?page_id=28" class="btn green bold">Conheça o processo de compra</a>
+  </div>
+</div>
+<!-- PRÓXIMOS EVENTOS -->
+<div class="grey lighten-2" style="height: 800px">
+  <div class="container">
+    <div class="row">
+      <div class="col s12 margin70 marginb50">
+        <h1 class="padding20 texto-escuro bold">Próximos Eventos:</h1>
+        <h3 class="texto-menos-escuro fs25">Veja abaixo os principais eventos que acontecerão nos próximos dias:</h3>
+      </div>
+      <div class="col s12">
+        <div class="col s12 m4">
+          <div class="container-evento">
+            <img class="image-center" src="<?php bloginfo('template_url'); ?>/images/evento1.png" />
+            <h3 class="bold padding15">Conferência do Espírito Santo Lagoinha</h3>
+            <p>Conferência na Igreja Batista da Lagoinha</p>
+          </div>
+          <a href="#" class="btn btn-destaque grey-text">Mais informações</a>
+        </div>
+        <div class="col s12 m4">
+          <div class="container-evento">
+            <img class="image-center" src="<?php bloginfo('template_url'); ?>/images/evento2.png" />
+            <h3 class="bold padding15">Conferência Sounds</h3>
+            <p>Conferência de Louvor e Adoração na Comunidade Alcance Curitiba, PR</p>
+          </div>
+          <a href="#" class="btn btn-destaque grey-text">Mais informações</a>
+        </div>
+        <div class="col s12 m4">
+          <div class="container-evento">
+            <img class="image-center" src="<?php bloginfo('template_url'); ?>/images/evento3.png" />
+            <h3 class="bold padding15">Conferência Profética</h3>
+            <p>Conferência Profética na sede da IEQ - Belém/PA</p>
+          </div>
+          <a href="#" class="btn btn-destaque grey-text">Mais informações</a>
+        </div>
+      </div>
     </div>
+  </div>
+</div>
+<!-- ENCONTRE OS EVENTOS -->
+<div class="hide-on-small-only parallax-container" style="height: 700px">
+  <div class="container">
+    <div class="row">
+      <div class="col s8 push-s2 margin50 marginb50">
+        <h1 class="padding40 texto-escuro bold">Encontre os Eventos mais próximos:</h1>
+        <br>
+        <a href="#" class="btn btn-destaque texto-amarelo">Ver todos os Eventos</a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- NEWSLETTER -->
+<div class="hide-on-small-only parallax-container">
+  <div class="texto-por-cima-parallax">
+    <div class="row">
+      <div class="col s8 push-s2 margin50 marginb50">
+        <h1 class="texto-amarelo bold">Deseja ficar por dentro das novidades?</h1>
+        <h3 class="white-text">
+          Se você deseja receber em seu e-mail todas as novidades do Avivando Brasil, assine nossa newsletter:
+        </h3>
+        <form class="newsletter">
+          <div class="row">
+            <div class="col s12">
+              <div class="col s12 m5 paddingr0">
+                <input type="text" placeholder="Nome completo" />
+              </div>
+              <div class="col s12 m5 paddingr0">
+                <input type="text" placeholder="Seu e-mail" />
+              </div>
+              <div class="col s12 m2 right">
+                <button class="btn-destaque white-text" type="submit">ENVIAR</button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <div class="parallax">
+    <img src="<?php bloginfo('template_url'); ?>/images/newsletter.jpg" />
   </div>
 </div>
 
-<?php get_footer('home'); ?>
+<?php get_footer(); ?>
